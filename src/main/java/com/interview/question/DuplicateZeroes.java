427e6274324d0b4067e6b42b39d31718f2daabf7
shishopr <prarthna.shishodia@hcltech.com>
2025-12-24 03:14:40 +0100

    new buggy file

--- src/main/java/com/interview/question/DuplicateZeroes.java
+++ src/main/java/com/interview/question/DuplicateZeroes.java
@@ -7,17 +7,18 @@ public class DuplicateZeroes {
     public static void main(String args[]) {
         int[] arr = {1, 0, 3, 0, 9, 0, 5, 4};
     }
-    int [] duplicateZeroes(int[] arr)
-    {
-     List<Integer> array=new ArrayList<>();
-     for(int i:arr) {
-         if (i == 0) {
-             array.add(i);
-             array.add(0);
-         } else {
-             array.add(i);
-         }
-     }
-     int res[]=new int[array.size()];
-     return array.toArray(res);
+
+    Object[] duplicateZeroes(int[] arr) {
+        List<Integer> array = new ArrayList<>();
+        for (int i : arr) {
+            if (i == 0) {
+                array.add(i);
+                array.add(0);
+            } else {
+                array.add(i);
+            }
+        }
+        int res[] = new int[array.size()];
+        return array.toArray();
+    }
 }
